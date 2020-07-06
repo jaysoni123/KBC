71e216f7c5db9de5b358f700459d67d945ad9546
Dhananjay Choksi <soni.jay008@gmail.com>
2020-07-06 20:57:11 +0530

    Updates

---
+++ precache-manifest.276089b7d0337f0966ad071539e56a04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b5f7607a6c58c58b461fd389393410",
+    "url": "/KBC/index.html"
+  },
+  {
+    "revision": "5201c268a80fc50e2f23",
+    "url": "/KBC/static/css/main.ea842193.chunk.css"
+  },
+  {
+    "revision": "36af60a06b35c1b039a8",
+    "url": "/KBC/static/js/2.6313b6b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KBC/static/js/2.6313b6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5201c268a80fc50e2f23",
+    "url": "/KBC/static/js/main.bcab5c83.chunk.js"
+  },
+  {
+    "revision": "ad93255ff0e23596bce7",
+    "url": "/KBC/static/js/runtime-main.c09fad33.js"
+  },
+  {
+    "revision": "bdbc7ea6a05af3f02ea1736c3736c76c",
+    "url": "/KBC/static/media/good_luck.bdbc7ea6.gif"
+  },
+  {
+    "revision": "cd5df42906a9bfe3dcfcfc73d6fb744d",
+    "url": "/KBC/static/media/kbc.cd5df429.png"
+  }
+]);
